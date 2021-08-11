a8a298c251f7536724b5b1d2f6228960494393df
idanmorem <37801660+idanmorem@users.noreply.github.com>
2021-08-11 21:06:51 +0300

    Update AdjacentList.cpp

--- AdjacentList.cpp
+++ AdjacentList.cpp
@@ -21,4 +21,35 @@ AdjacentList::ListNode *AdjacentList::getHead() const {
     return head;
 }
 
+void AdjacentList::deleteNode(ListNode* previousNode)
+{
+    if(previousNode == nullptr)
+        deleteFromBeginningOfList();
+    else if(previousNode->getNext() == tail)
+        deleteFromEndOfList(previousNode);
+    else
+        deleteFromInnerPlaceInList(previousNode);
+}
+
+void AdjacentList::deleteFromBeginningOfList()
+{
+    ListNode* nodeToDelete = head;
+    head = head->getNext();
+    delete nodeToDelete;
+    if(head == nullptr)
+        tail = nullptr;
+}
+void AdjacentList::deleteFromEndOfList(ListNode* previousNode)
+{
+    delete previousNode->getNext();
+    tail = previousNode;
+    previousNode->setNext(nullptr);
+}
+void AdjacentList::deleteFromInnerPlaceInList(ListNode* previousNode)
+{
+    ListNode* nodeToDelete = previousNode->getNext();
+
+    previousNode->setNext(nodeToDelete->getNext());
+    delete nodeToDelete;
+}
 
